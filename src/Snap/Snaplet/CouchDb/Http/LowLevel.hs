de561145227bd324c8d6f711da7633d88caf6602
flux0r <flux0r@brettmingalls.com>
2013-09-22 17:59:12 -0700

    Fix small oops.

--- src/Snap/Snaplet/CouchDb/Http/LowLevel.hs
+++ src/Snap/Snaplet/CouchDb/Http/LowLevel.hs
@@ -19,8 +19,6 @@ import System.Posix.Internals (setNonBlockingFD)
 import qualified Data.Machine as M
 import           Data.Machine (SourceT)
 
-main = undefined
-
 foreign import ccall unsafe "socket"
     c_socket :: CInt -> CInt -> CInt -> IO CInt
 
@@ -43,18 +41,12 @@ getSock fam typ pro =
                 sockStat <- newMVar NotConnected
                 return . Right $ MkSocket fd fam typ pro sockStat
 
-source sock = M.repeatedly $ do
-    bs <- liftIO $ NS.recv sock 4096
-    if B.null bs
-        then M.stop
-        else M.yield bs
-
-srcSock :: MonadIO m
-        => Socket
-        -> Ptr CChar
-        -> CSize
-        -> M.SourceT m (Either Errno ByteString)
-srcSock s mem nbytes = M.repeatedly $ do
+srcSocket :: MonadIO m
+          => Socket
+          -> Ptr CChar
+          -> CSize
+          -> M.SourceT m (Either Errno ByteString)
+srcSocket s mem nbytes = M.repeatedly $ do
     x <- liftIO $ fdRecv fd mem nbytes
     case x of
         Right 0 -> M.stop
@@ -65,14 +57,14 @@ srcSock s mem nbytes = M.repeatedly $ do
   where
     fd = fdSocket s
 
-checkForeign :: (Integral a) => a -> IO (Either Errno Int)
+checkForeign :: (Integral a, Num b) => a -> IO (Either Errno b)
 checkForeign x =
     if x == -1
         then getErrno >>= return . Left
         else return . Right . fromIntegral $ x
 
-fdRecv :: CInt -> Ptr CChar -> CSize -> IO (Either Errno Int)
+fdRecv :: Num a => CInt -> Ptr CChar -> CSize -> IO (Either Errno a)
 fdRecv fd ptr n = c_recv fd ptr n 0 >>= checkForeign
 
-fdRead :: Fd -> Ptr CChar -> CSize -> IO (Either Errno Int)
+fdRead :: Num a => Fd -> Ptr CChar -> CSize -> IO (Either Errno a)
 fdRead (Fd fd) ptr n = c_read fd ptr n >>= checkForeign
