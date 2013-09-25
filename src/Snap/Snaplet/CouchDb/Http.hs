14ae00eea77bb19ce921301707248b0a3662ece4
flux0r <flux0r@brettmingalls.com>
2013-09-24 23:42:03 -0700

    Work on http.

--- src/Snap/Snaplet/CouchDb/Http.hs
+++ src/Snap/Snaplet/CouchDb/Http.hs
@@ -1,12 +1,41 @@
 module Snap.Snaplet.CouchDb.Http where
 
+import Control.Monad.IO.Class (MonadIO)
+import Data.Time.Clock (UTCTime, NominalDiffTime, diffUTCTime)
+import Control.Exception (throwIO)
 import Snap.Snaplet.CouchDb.Http.Cookies (freshenCookies)
 import Snap.Snaplet.CouchDb.Http.Types (RequestBody (RequestBodyProducer),
-                                        reqBody)
+                                        Request (..), HttpError)
 import Snap.Snaplet.CouchDb.Utils (liftedNow)
+import qualified Data.Map as Map
+import System.Timeout (timeout)
 
--- httpRaw req sock httpApi t =
---     let (req', coks) = freshenCookies t req httpApi
 
-b req = case reqBody req of
-    RequestBodyProducer cl prod -> (Just cl, prod) 
+httpNoRedirects req' resource = let cs = reqCookies req' in
+    do
+        (req, cs') <-
+            if Map.null cs
+                then return (req', cs)
+                else return $ liftedNow >>= \t -> freshenCookies t req' True
+        return (req, cs')
+        (t', res) <- connWrap (reqTimeout req)
+                              (reqFailedConnection req)
+
+connWrap :: Maybe Int -> e -> IO a -> IO (Maybe NominalDiffTime, Either e a)
+connWrap Nothing _ f    = fmap (\x -> (Nothing, Right x)) f
+connWrap (Just t) e f   = liftedNow >>= \t0 ->
+    timeout t f >>= \res-> timeoutHandler (fromIntegral t) t0 res e
+
+timeoutHandler :: MonadIO m
+               => NominalDiffTime
+               -> UTCTime
+               -> Maybe a
+               -> e
+               -> m (Maybe NominalDiffTime, Either e a)
+timeoutHandler _ _ Nothing e    = return (Nothing, Left e)
+timeoutHandler t t0 (Just r) e  = liftedNow >>= \t1 ->
+    let dt  = (diffUTCTime t1 t0)*1000*1000
+        t'  = t - dt
+    in  if t' <= 0
+           then return (Nothing, Left e)
+           else return (Just t', Right r)
--- src/mod.hs
+++ src/mod.hs
@@ -1,9 +1,1 @@
-import Snap.Snaplet.CouchDb.Http.Cookies
-import Snap.Snaplet.CouchDb.Http.Types
-import Snap.Snaplet.CouchDb.Utils
 import Snap.Snaplet.CouchDb.Http
-
-import qualified Data.Map as Map
-
-
-import Snap.Snaplet.CouchDb.Http.Request
